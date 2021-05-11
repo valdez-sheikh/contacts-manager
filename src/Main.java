7bcc19a7ad02bf3f414fd43454ad3a9c3137d78e
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-11 14:08:22 -0500

    need to work on delete and exemptions

--- src/Main.java
+++ src/Main.java
@@ -13,34 +13,35 @@ public class Main {
 //        Contact roland = new Contact("Roland", "Valdez", "5557777777");
 //        System.out.println(irfa.getContact());
 //        System.out.println(roland.getContact());
-        DisplayMenu();
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        if(userInput.equals("1")){
+        boolean repeat = true;
+        do {
+            Path pathToContacts = Paths.get("./data/contacts.txt");
+            DisplayMenu();
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            if (userInput.equals("1")) {
+                displayContacts(pathToContacts);
+                System.out.println();
+            } else if (userInput.equals("2")) {
+                System.out.println("Can you enter the Full Name and number?");
+                String contact = scanner.nextLine();
+                addContact(contact, pathToContacts);
+                System.out.println();
+                displayContacts(pathToContacts);
+                System.out.println();
+            } else if (userInput.equals("3")) {//search contact
+                System.out.println("Enter the name of the contact");
+                String search = scanner.nextLine();
+                searchContacts(search, pathToContacts);
+            } else if (userInput.equals("4")) {// delete contact
 
-        } else if (userInput.equals("2")){
-
-        } else if (userInput.equals("3")){
-
-        } else if (userInput.equals("4")){
-
-        } else if (userInput.equals("5")){
-
-        }
-
-
-
-        Path pathToContacts = Paths.get("./data/contacts.txt");
-        List<String> contactList = Files.readAllLines(pathToContacts); //reading each line and storing each line into a list
-        System.out.println("Contact List:");
-        for (int i = 0; i < contactList.size(); i += 1) {
-            System.out.println((i + 1) + ": " + contactList.get(i)); // loops through the list in the text file
-        }
-        Files.write(pathToContacts, Arrays.asList("\nDouglas Hirsh 7777777777"), StandardOpenOption.APPEND);
-
+            } else if (userInput.equals("5")) {//exit
+                repeat = false;
+            }
+        } while (repeat);
 
     }
-
+/////////////// OUTSIDE MAIN
     public static void DisplayMenu(){
         System.out.println("1. View contacts." +
                 "\n2. Add a new contact. " +
@@ -49,5 +50,28 @@ public class Main {
                 "\n5. Exit. " +
                 "\nEnter an option (1, 2, 3, 4 or 5):"); // this displays the menu..in order for anything to run you have to call it at the top in the main method
     }
-
+    public static void displayContacts (Path pathToContacts) throws IOException {
+        List<String> contactList = Files.readAllLines(pathToContacts); //reading each line and storing each line into a list
+        System.out.println("Contact List:");
+        for (int i = 0; i < contactList.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactList.get(i)); // loops through the list in the text file
+        }
+    }
+    public static void addContact(String contact, Path pathToContacts) throws IOException {
+        Files.write(pathToContacts, Arrays.asList(contact), StandardOpenOption.APPEND);
+    }
+    public static void searchContacts(String search, Path pathToContacts) throws IOException {
+        boolean foundEntry = false;
+        List<String> contactList = Files.readAllLines(pathToContacts); //reading each line and storing each line into a list
+        for (int i = 0; i < contactList.size(); i += 1) {
+            if (contactList.get(i).contains(search)) {
+                System.out.println(contactList.get(i));
+                foundEntry = true;
+                break;
+            }
+        }
+        if (!foundEntry){
+            System.out.println("The name you searched is not in our contact list!");
+        }
+    }
 }
